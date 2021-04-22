9de0bb02cf0d31214b2cbb4973f56928bc4d781e
hodongman <jhd9206@gmail.com>
2021-04-23 01:05:18 +0900

    first commit

---
+++ TodoApplication/Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TodoApplication.Data;
+
+namespace TodoApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class TaskController : Controller
+    {
+        private readonly TaskRepository _taskRepository;
+
+        public TaskController(TaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _taskRepository.GetAllTasksAsync();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Models.Task task)
+        {
+            var result = await _taskRepository.SaveAsync(task);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ TodoApplication/Data/TaskRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+using MySqlConnector;
+using Dapper;
+
+using TodoApplication.Core;
+
+
+namespace TodoApplication.Data
+{
+    public class TaskRepository : IDisposable
+    {
+        private readonly IDbConnection _db;
+
+        public TaskRepository(IOptions<ConnectionStrings> connectionStrings)
+        {
+            _db = new MySqlConnection(connectionStrings.Value.TodoDatabase);
+        }
+
+        public void Dispose()
+        {
+            _db.Close();
+        }
+
+        public Task<IEnumerable<Models.Task>> GetAllTasksAsync()
+        {
+            return _db.QueryAsync<Models.Task>("SELECT * FROM tasks");
+        }
+
+        public Task<int> SaveAsync(Models.Task task)
+        {
+            if (task.Id.HasValue)
+            {
+                string update = @"UPDATE tasks SET description = @Description, completed = @Completed WHERE id = @Id";
+                return _db.ExecuteAsync(update, new { task.Description, task.IsCompleted, task.Id });
+            }
+            else
+            {
+                string insert = @"INSERT INTO tasks (description) VALUES (@Description)";
+                return _db.ExecuteAsync(insert, new { task.Description });
+            }
+        }
+
+        public Task<int> DeleteAsync(int taskId)
+        {
+            string delete = @"DELETE FROM tasks WHERE id = @Id";
+            return _db.ExecuteAsync(delete, new { Id = taskId });
+        }
+    }
+}
